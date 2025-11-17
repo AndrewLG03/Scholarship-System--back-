84f88350b3e233bc06e812e0d741752ea1ec55f7
ana-19 <62568475+ana-19@users.noreply.github.com>
2025-11-17 15:18:06 -0600

    Update roleMiddleware.js

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,20 +1,9 @@
 function onlyCommittee(req, res, next) {
-  try {
-    // Si no viene un usuario en el token
-    if (!req.user) {
-      return res.status(401).json({ error: "No autorizado" });
+    if (!req.user || req.user.role !== "comite") {
+        return res.status(403).json({ message: "Acceso denegado: solo comité" });
     }
-
-    // Validar que el rol del usuario sea Comité
-    if (req.user.role !== "comite") {
-      return res.status(403).json({ error: "Acceso denegado. Solo comité." });
-    }
-
-    next(); // El usuario tiene rol de comité → continúa
-  } catch (error) {
-    console.error("Error en control de roles:", error);
-    res.status(500).json({ error: "Error interno de permisos" });
-  }
+    next();
 }
 
 module.exports = { onlyCommittee };
+
