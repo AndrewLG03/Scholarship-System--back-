315be5af86b7040b95ea1aca16f764bb8a53ed41
ana-19 <62568475+ana-19@users.noreply.github.com>
2025-11-17 15:15:00 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,26 @@
-// src/config/database.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT || 3306),
+    host: "tiusr36pl.cuc-carrera-ti.ac.cr",
+    user: "ProyectoDB",
+    password: "Proyecto_SQL",
+    database: "BecasDB",
+    port: 3306,
     waitForConnections: true,
-    connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
+    connectionLimit: 10
     queueLimit: 0
-  // Si el servidor requiere SSL, agregar la opción ssl aquí
 });
+
+// Test de conexión
+(async () => {
+    try {
+        await pool.query("SELECT 1");
+        console.log("MySQL pool OK.");
+    } catch (error) {
+        console.error("Error conectando a MySQL:", error);
+    }
+})();
+
 
 module.exports = { pool };
