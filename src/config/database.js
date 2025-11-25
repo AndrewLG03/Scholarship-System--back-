ecf0eed6d04ca4f11a6c1001a22f543f96974b75
AndrewLG03 <loaiza29andres@gmail.com>
2025-11-25 00:27:39 -0600

    Fix: conflictos eliminados

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-// src/config/database.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT || 3306),
-    waitForConnections: true,
-    connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
-    queueLimit: 0
-  // Si el servidor requiere SSL, agregar la opción ssl aquí
-});
-
-module.exports = { pool };
-=======
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -31,4 +12,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = { pool };
->>>>>>> 55fd35a4906540faf3aab4b4a3a4b9a73372fd77
